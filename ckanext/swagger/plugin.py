14ca7e12adaeafa119bb57c66cb63e59b4ae9c80
Emilio José Pardo Pérez <emiliopardo@guadaltel.com>
2024-10-17 13:31:26 +0200

    corrección de codigo para ckan 2.9

--- ckanext/swagger/plugin.py
+++ ckanext/swagger/plugin.py
@@ -1,61 +1,52 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from flask import jsonify
+from flask import Blueprint, jsonify, render_template
 
+# Definimos el blueprint de Flask
+swagger_blueprint = Blueprint('swagger', __name__)
 
+# Ruta para la UI de Swagger
+@swagger_blueprint.route('/swagger', methods=['GET'])
+def swagger_ui():
+    return toolkit.render('swagger_ui.html')
+
+# Ruta para el JSON de Swagger
+@swagger_blueprint.route('/swagger.json', methods=['GET'])
+def swagger_json():
+    api_base_url = toolkit.config.get('ckan.site_url') + '/api/3'
+    swagger_json_data = {
+        "openapi": "3.0.0",
+        "info": {
+            "title": "CKAN API",
+            "version": "1.0.0"
+        },
+        "paths": {
+            "/": {
+                "get": {
+                    "description": "Retrieve API information",
+                    "responses": {
+                        "200": {
+                            "description": "OK"
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return jsonify(swagger_json_data)
+
+# El plugin principal
 class SwaggerPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
 
-    # IConfigurer
-
+    # Método para devolver el blueprint (Flask) que maneja las rutas
     def get_blueprint(self):
-        return [
-            {
-                "url": "/swagger",
-                "controller": SwaggerController,
-                "action": "swagger_ui",
-                "methods": ["GET"]
-            }
-        ]
-    
-    def update_config_schema(self, schema):
-        return schema
+        return swagger_blueprint
 
-    def modify_navigation(self, nav_menu):
-        nav_menu.add(_('API Docs'), h.url_for('swagger_ui'))
-        return nav_menu
-
+    # Método para configurar los directorios de plantillas, recursos públicos, etc.
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'swagger')
+        toolkit.add_resource('fanstatic', 'swagger')
 
-class SwaggerController(plugins.toolkit.BaseController):
-    def swagger_ui(self):
-        return tk.render('swagger_ui.html')
-
-    def swagger_json(self):
-        api_base_url = tk.config.get('ckan.site_url') + '/api/3'
-        swagger_json = {
-            "openapi": "3.0.0",
-            "info": {
-                "title": "CKAN API",
-                "version": "1.0.0"
-            },
-            "paths": {
-                "/": {
-                    "get": {
-                        "description": "Retrieve API information",
-                        "responses": {
-                            "200": {
-                                "description": "OK"
-                            }
-                        }
-                    }
-                },
-                # Add other endpoints dynamically
-            }
-        }
-        return jsonify(swagger_json)
