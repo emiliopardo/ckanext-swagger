1b61e79a0b0d473390434af114dc20d9539297f5
Emilio José Pardo Pérez <emiliopardo@guadaltel.com>
2024-10-17 14:36:00 +0200

    correcciones plugin para generación json dinamico de endpoints

--- ckanext/swagger/plugin.py
+++ ckanext/swagger/plugin.py
@@ -1,29 +1,21 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, current_app
 
-# Definimos el blueprint de Flask
 swagger_blueprint = Blueprint('swagger', __name__)
 
-# Ruta para la UI de Swagger
-@swagger_blueprint.route('/swagger', methods=['GET'])
-def swagger_ui():
-    return toolkit.render('swagger_ui.html')
-
-# Ruta para el JSON de Swagger
 @swagger_blueprint.route('/swagger.json', methods=['GET'])
 def swagger_json():
     api_base_url = toolkit.config.get('ckan.site_url') + '/api/3'
-    swagger_json_data = {
-        "openapi": "3.0.0",
-        "info": {
-            "title": "CKAN API",
-            "version": "1.0.0"
-        },
-        "paths": {
-            "/": {
+    
+    # Obtener las rutas activas de CKAN desde la aplicación Flask
+    paths = {}
+    for rule in current_app.url_map.iter_rules():
+        if rule.rule.startswith('/api'):  # Solo incluimos las rutas que empiezan por '/api'
+            # Construimos la entrada para Swagger en base a la ruta
+            path_data = {
                 "get": {
-                    "description": "Retrieve API information",
+                    "description": f"Endpoint for {rule.endpoint}",
                     "responses": {
                         "200": {
                             "description": "OK"
@@ -31,8 +23,23 @@ def swagger_json():
                     }
                 }
             }
-        }
+            paths[rule.rule] = path_data
+
+    # Generar el archivo Swagger JSON de forma dinámica
+    swagger_json_data = {
+        "openapi": "3.0.0",
+        "info": {
+            "title": "CKAN API",
+            "version": "1.0.0"
+        },
+        "servers": [
+            {
+                "url": api_base_url
+            }
+        ],
+        "paths": paths
     }
+
     return jsonify(swagger_json_data)
 
 # El plugin principal
@@ -40,13 +47,10 @@ class SwaggerPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
 
-    # Método para devolver el blueprint (Flask) que maneja las rutas
     def get_blueprint(self):
         return swagger_blueprint
 
-    # Método para configurar los directorios de plantillas, recursos públicos, etc.
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'swagger')
-
