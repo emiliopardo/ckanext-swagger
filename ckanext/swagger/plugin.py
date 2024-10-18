fd7dc78489e05b4ca19ec3380c3baa5005e95bf0
Emilio José Pardo Pérez <emiliopardo@guadaltel.com>
2024-10-18 13:15:27 +0200

    cambiado plugin

--- ckanext/swagger/plugin.py
+++ ckanext/swagger/plugin.py
@@ -1,12 +1,12 @@
 import os
 from flask import Blueprint, send_from_directory
 from ckan.plugins import SingletonPlugin, implements
-from ckan.plugins.interfaces import IRoutes, IConfigurer
-from ckan.plugins.toolkit import config
+from ckan.plugins.interfaces import IBlueprint, IConfigurer
+from ckan.plugins.toolkit import config, add_template_directory, add_public_directory, add_resource
 
 class SwaggerPlugin(SingletonPlugin):
-    implements(IRoutes, inherit=True)
     implements(IConfigurer, inherit=True)
+    implements(IBlueprint, inherit=True)
 
     def update_config(self, config):
         """
@@ -14,13 +14,13 @@ class SwaggerPlugin(SingletonPlugin):
         Se utiliza para agregar directorios de plantillas, estáticos y configuraciones.
         """
         # Agregar directorio de plantillas
-        self.add_template_directory(config, 'templates')
+        add_template_directory(config, 'templates')
 
         # Agregar el directorio público (archivos estáticos como CSS y JS)
-        self.add_public_directory(config, 'public')
+        add_public_directory(config, 'public')
 
-        # Agregar recursos (si tienes algo adicional)
-        self.add_resource('public', 'swagger')
+        # Agregar recursos estáticos adicionales si es necesario
+        add_resource('public', 'swagger')
 
     def get_blueprint(self):
         """
@@ -38,21 +38,8 @@ class SwaggerPlugin(SingletonPlugin):
         # Ruta para servir Swagger UI
         @swagger_blueprint.route('/swagger')
         def swagger_ui():
-            return send_from_directory(os.path.join(os.path.dirname(__file__), 'public', 'swagger-ui'), 'index.html')
+            # Directorio donde se encuentra el index.html de Swagger UI
+            static_folder = os.path.join(os.path.dirname(__file__), 'public', 'swagger-ui')
+            return send_from_directory(static_folder, 'index.html')
 
         return swagger_blueprint
-
-    def before_map(self, map):
-        """
-        Este método es parte de la interfaz IRoutes. Aquí puedes agregar nuevas rutas.
-        """
-        # Aquí puedes mapear las rutas de Flask para asegurar que se registren
-        map.connect('/swagger', controller='ckanext.swagger.plugin:SwaggerPlugin', action='swagger_ui')
-        map.connect('/swagger.json', controller='ckanext.swagger.plugin:SwaggerPlugin', action='swagger_json')
-        return map
-
-    def after_map(self, map):
-        """
-        Si necesitas modificar el mapa de rutas después de que CKAN lo haya procesado.
-        """
-        return map
